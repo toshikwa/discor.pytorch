fix numerical instabilities

discor/algorithm/discor.py
@@ -4,7 +4,7 @@ from torch.optim import Adam
 
 from .sac import SAC
 from discor.network import TwinnedStateActionFunction
-from discor.utils import disable_gradients, soft_update, update_params
+from discor.utils import disable_gradients, soft_update, update_params, softmax
 
 
 class DisCor(SAC):
@@ -93,15 +93,15 @@ class DisCor(SAC):
             next_errs1, next_errs2 = \
                 self._target_error_net(next_states, next_actions)
 
-        # Numerators inside the exponent of importance weights.
-        nums1 = (1.0 - dones) * self._gamma * next_errs1
-        nums2 = (1.0 - dones) * self._gamma * next_errs2
+        # Terms inside the exponent of importance weights.
+        x1 = -(1.0 - dones) * self._gamma * next_errs1 / self._tau1
+        x2 = -(1.0 - dones) * self._gamma * next_errs2 / self._tau2
 
         # Calculate self-normalized importance weights.
-        imp_ws1 = torch.exp(- nums1 / self._tau1)
-        imp_ws2 = torch.exp(- nums2 / self._tau2)
+        imp_ws1 = softmax(x1)
+        imp_ws2 = softmax(x2)
 
-        return imp_ws1 / imp_ws1.sum(), imp_ws2 / imp_ws2.sum()
+        return imp_ws1, imp_ws2
 
     def calc_current_errors(self, states, actions):
         curr_errs1, curr_errs2 = self._online_error_net(states, actions)

discor/algorithm/sac.py
@@ -4,7 +4,8 @@ from torch.optim import Adam
 
 from .base import Algorithm
 from discor.network import TwinnedStateActionFunction, GaussianPolicy
-from discor.utils import disable_gradients, soft_update, update_params
+from discor.utils import disable_gradients, soft_update, update_params, \
+    assert_action
 
 
 class SAC(Algorithm):
@@ -59,14 +60,18 @@ class SAC(Algorithm):
             state[None, ...], dtype=torch.float, device=self._device)
         with torch.no_grad():
             action, _, _ = self._policy_net(state)
-        return action.cpu().numpy()[0]
+        action = action.cpu().numpy()[0]
+        assert_action(action)
+        return action
 
     def exploit(self, state):
         state = torch.tensor(
             state[None, ...], dtype=torch.float, device=self._device)
         with torch.no_grad():
             _, _, action = self._policy_net(state)
-        return action.cpu().numpy()[0]
+        action = action.cpu().numpy()[0]
+        assert_action(action)
+        return action
 
     def update_target_networks(self):
         soft_update(

discor/env.py
@@ -1,4 +1,3 @@
-import numpy as np
 import gym
 from gym import wrappers
 from gym.envs.registration import register
@@ -19,18 +18,9 @@ METAWORLD_TASKS = (
     'stick-pull-v1', 'dial-turn-v1', 'peg-insert-side-v1')
 
 for task in METAWORLD_TASKS:
-
-    class StableEnv(ALL_ENVIRONMENTS[task]):
-        def step(self, action):
-            action = np.clip(
-                action, self.action_space.low + 1e-8,
-                self.action_space.high - 1e-8)
-            obs, reward, done, info = super().step(action)
-            return obs, reward, done, info
-
     register(
         id=task,
-        entry_point=StableEnv,
+        entry_point=ALL_ENVIRONMENTS[task],
         max_episode_steps=150)
     assert_env(gym.make(task))
 